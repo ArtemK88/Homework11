1a3d36830650aedbead2e29f923b19691aff1ffa
ArtemK88 <kapiton2007@gmail.com>
2023-06-21 20:49:29 +0300

    Words1

--- src/Words1.java
+++ src/Words1.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Words1 {
+//1. Є рядок із текстом англійською мовою. Виділити всі унікальні слова.
+//   Слова, які відрізняються лише регістром букв, вважати однаковими.
+//   Використовуйте клас HashSet.
+import java.util.HashSet;
+
+
+public class Words1 {
+    public static void main(String[] args) {
+        String text = "Hello world hello World is Is a A the The ";
+        text = text.replaceAll("[^a-zA-Z ]", "");
+        HashSet<String> uniqueWords = new HashSet<>();
+        String[] words = text.split("\\W+");
+        for (String word : words) {
+            String lowercaseWord = word.toLowerCase();
+            uniqueWords.add(lowercaseWord);
+        }
+        for (String word : uniqueWords) {
+            System.out.println(word);
+        }
+    }
 }
+
--- src/Words2.java
+++ src/Words2.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Words2 {
+//Є рядок із текстом англійською мовою. Виділити всі унікальні слова.
+// Для кожного слова підрахувати частоту його входження в текст. Слова, які
+// відрізняються регістром літер, вважати різними. Використовувати клас
+// HashMap.
+import java.util.HashMap;
+
+public class Words2 {
+
+    public static void main(String[] args) {
+        String text = "Hello world hello World is Is a A the The";
+        String[] words = text.split(" ");
+        HashMap<String, Integer> wordFrequency = new HashMap<>();
+        for (String word : words) {
+            if (wordFrequency.containsKey(word)) {
+                int frequency = wordFrequency.get(word);
+                wordFrequency.put(word, frequency + 1);
+            } else {
+                wordFrequency.put(word, 1);
+            }
+        }
+        for (String word : wordFrequency.keySet()) {
+            int frequency = wordFrequency.get(word);
+            System.out.println(word + ": " + frequency);
+        }
+    }
 }
